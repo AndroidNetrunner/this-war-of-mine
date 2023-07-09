8632721cea78214ce81a933692655d8d0af538f2
AndroidNetrunner <kby9906@naver.com>
2023-07-09 22:15:41 +0900

    [FIX] ResourceStatus interface를[key in ResourceName]으로 수정

--- src/interfaces/ResourceStatus.ts
+++ src/interfaces/ResourceStatus.ts
@@ -1,38 +1,5 @@
+import { ResourceName } from "../redux/Slices/resourceSlice";
+
 export default interface ResourceStatus {
-  wood: number;
-  component: number;
-  water: number;
-  alcheol: number;
-  moonshine: number;
-  cigarette: number;
-  sugar: number;
-  coffee: number;
-  vegetable: number;
-  rawFood: number;
-  cannedFood: number;
-  shovel: number;
-  lockpick: number;
-  filter: number;
-  mechanicalPart: number;
-  electricalPart: number;
-  jewelry: number;
-  book: number;
-  brokenGuitar: number;
-  guitar: number;
-  weaponPart: number;
-  brokenPistol: number;
-  pistol: number;
-  knife: number;
-  shell: number;
-  brokenAssaultRifle: number;
-  assaultRifle: number;
-  hatchet: number;
-  ammo: number;
-  brokenShotgun: number;
-  shotgun: number;
-  herb: number;
-  chems: number;
-  herbalMeds: number;
-  meds: number;
-  bandages: number;
+  [key in ResourceName]: number;
 }
